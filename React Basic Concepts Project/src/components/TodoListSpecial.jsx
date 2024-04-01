aec4750da165382c6daf5efc014fc58a6e851f40
Bees7nees <150014677+Bees7nees@users.noreply.github.com>
2024-04-01 11:58:27 +0100

    Create TodoListSpecial.jsx

---
+++ React Basic Concepts Project/src/components/TodoListSpecial.jsx
@@ -0,0 +1,17 @@
+export function TodoListSpecial() {
+  const [todos, setTodos] = useState([]);
+
+  return (
+    <>
+      <div>
+        <h2>Special Todo List</h2>
+        <ul></ul>
+          </div>
+          <form action="">
+            <input type="text" />
+            <button type="submit">Add</button>
+            <button type="reset">Clear</button>
+          </form>
+    </>
+  );
+}
